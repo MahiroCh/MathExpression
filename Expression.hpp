f8858a8e36d08a759743edacb3c8d0b18112a935
MahiroCh <mahirostuff@gmail.com>
2025-03-10 23:52:22 +0300

    Final changes

--- Expression.hpp
+++ Expression.hpp
@@ -1,5 +1,5 @@
-#ifndef EXPRESSION_H
-#define EXPRESSION_H
+#ifndef EXPR_H
+#define EXPR_H
 
 #include <memory>
 #include <string>
@@ -12,103 +12,253 @@
 #include <cmath>
 #include <complex>
 #include <type_traits>
+#include <sstream>
 
 template <typename T>
 class Expression {
-
 public:
-    
+
+    // ---------------------------------------------------------------------------------------------------- //
     // КОНСТРУКТОРЫ И ДЕСТРУКТОРЫ
+    // ---------------------------------------------------------------------------------------------------- //
 
-    Expression(); // Для объявления пустых переменных
-    Expression(const char* arg); // Конструктор для строки-выражения
-    Expression(const T& arg); // Конструктор для числа
-    Expression(const Expression<T>& other); // Конструктор копирования
+    /*
+    Конструктор для объявления переменной без выражения.
+    */
+    Expression(); 
+
+    /*
+    Конструктор выражения из строки.
+    */
+    Expression(const char*); 
+
+    /*
+    Конструктор выражения из числа.
+    */
+    Expression(const T&); 
+
+    /*
+    Конструктор копирования.
+    */
+    Expression(const Expression<T>&);
+
+    /*
+    Конструктор перемещения.
+    */
+    Expression(Expression<T>&& other) noexcept;
     
+    // ---------------------------------------------------------------------------------------------------- //
     // ПОЛЬЗОВАТЕЛЬСКИЕ МЕТОДЫ
+    // ---------------------------------------------------------------------------------------------------- //
+
+    /*
+    Выражение в строку.
+    */
+    std::string toString() const;
+
+    /*
+    Замена переменных.
+    */
+    void subsVar(const std::string&);
+
+    /*
+    Вычислить значение выражения.
+    */
+    T evaluate() const;
+
+    /*
+    Продифференцировать по переменной.
+    */
+    Expression<T> differentiate(const std::string&) const;
     
-    void debugAST() const; // Вывод AST для дебага
-    std::string toString() const; // Выражение в строку
-    void subsVar(const std::string& varStr); // Подстановка значений
-    T evaluate() const; // Вычисление выражения
+    /*
+    Для дебага.: Вывод АСТ-дерева.
+    */
+    void debugAST() const;
 
+    // ---------------------------------------------------------------------------------------------------- //
     // ОПЕРАТОРЫ ДЛЯ ТИПА EXPRESSION
+    // ---------------------------------------------------------------------------------------------------- //
 
-    Expression<T> operator+(const Expression<T>& rhs);
-    Expression<T> operator-(const Expression<T>& rhs); 
-    Expression<T> operator*(const Expression<T>& rhs);
-    Expression<T> operator/(const Expression<T>& rhs);
-    Expression<T> operator^(const Expression<T>& rhs);
-    Expression<T>& operator=(const Expression<T>& other); // Оператор присваивания
+    Expression<T> operator+(const Expression<T>&);
+    Expression<T> operator-(const Expression<T>&); 
+    Expression<T> operator*(const Expression<T>&);
+    Expression<T> operator/(const Expression<T>&);
+    Expression<T> operator^(const Expression<T>&);
+    Expression<T>& operator=(const Expression<T>&); // Оператор присваивания.
+    Expression<T>& operator=(Expression<T>&&) noexcept; // Оператор перемещения.
+
 
 private:
 
-    // УЗЛЫ AST
+    // ---------------------------------------------------------------------------------------------------- //
+    // AST (АБСТРАКТНОЕ СИНТАКСИЧЕСКОЕ ДЕРЕВО)
+    // ---------------------------------------------------------------------------------------------------- //
 
-    struct Node { // Абстрактный класс для узла AST
+    /*
+    Абстрактный класс для узла AST.
+    */
+    struct Node {
+
         virtual ~Node() = default;
-        virtual std::string toString() const = 0;
-        // virtual void print(int indent = 0) const = 0;
+        virtual std::string nodeToString() const = 0;
+        virtual void print(int indent = 0) const = 0; // Для дебага.
     };
 
-    std::unique_ptr<Node> root; // Корень AST
+    /*
+    Узел для чисел.
+    */
+    struct NumberNode : Node {
 
-    struct NumberNode : Node { // Узел для чисел
         T value;
-        NumberNode(T value) : value(value) {}
-        std::string toString() const override;
-        // void print(int indent) const override;
+        NumberNode(T value) : value{value} {}
+        std::string nodeToString() const override;
+        void print(int) const override; // Для дебага.
     };
 
-    struct VariableNode : Node { // Узел для переменных
+    /*
+    Узел для переменных.
+    */
+    struct VariableNode : Node {
+
         std::string name;
-        VariableNode(const std::string& name) : name(name) {}
-        std::string toString() const override;
-        // void print(int indent) const override;
+        VariableNode(const std::string& name) : name{name} {}
+        std::string nodeToString() const override;
+        void print(int) const override; // Для дебага.
     };
 
-    struct BinaryOperationNode : Node { // Узел для бинарных операций
+    /*
+    Узел для бинарных операций.
+    */
+    struct BinaryOperationNode : Node {
+
         char operation;
         std::unique_ptr<Node> left;
         std::unique_ptr<Node> right;
-        BinaryOperationNode(char operation, std::unique_ptr<Node> left, std::unique_ptr<Node> right)
-            : operation(operation), left(std::move(left)), right(std::move(right)) {}
-        std::string toString() const override;
-        // void print(int indent) const override;
+        BinaryOperationNode(char operation, std::unique_ptr<Node> left, std::unique_ptr<Node> right) 
+            : operation{operation}, left{std::move(left)}, right{std::move(right)} {}
+        std::string nodeToString() const override;
+        void print(int) const override; // Для дебага.
     };
 
-    struct FunctionNode : Node { // Узел для функций
+    /*
+    Узел для унарных операций.
+    */
+    struct UnaryOperationNode : Node {
+
+        char operation;
+        std::unique_ptr<Node> arg;
+        UnaryOperationNode(char operation, std::unique_ptr<Node> operand) 
+            : operation{operation}, arg{std::move(operand)} {}
+        std::string nodeToString() const override;
+        void print(int) const override; // Для дебага.
+    };
+
+    /*
+    Узел для функций.
+    */
+    struct FunctionNode : Node { 
+
         std::string function;
         std::unique_ptr<Node> arg;
-        FunctionNode(const std::string& function, std::unique_ptr<Node> arg)
-            : function(function), arg(std::move(arg)) {}
-        std::string toString() const override;
-        // void print(int indent) const override;
+        FunctionNode(const std::string& function, std::unique_ptr<Node> arg) 
+            : function{function}, arg{std::move(arg)} {}
+        std::string nodeToString() const override;
+        void print(int) const override; // Для дебага.
     };
+
+    /*
+    Корень АСТ-дерева.
+    */
+    std::unique_ptr<Node> root;
     
-    // ФУНКЦИИ ДЛЯ ПАРСИНГА
+    // ---------------------------------------------------------------------------------------------------- //
+    // НУЛЕВОЙ ЭТАП ПАРСИНГА СТРОКИ В ВЫРАЖЕНИЕ (ХРАНЕНИЕ В AST-ДЕРЕВЕ)
+    // ---------------------------------------------------------------------------------------------------- //
 
-    std::vector<std::string> tokenize(const std::string& expr); // Токенизация выражения для последующего парсинга
+    /*
+    Токенизация выражения для последующего парсинга.
+    */
+    std::vector<std::string> tokenize(const std::string&);
 
-    // // ПАРСИНГ МАТЕМАТИКИ В СООТВЕТСВИИ С PEMDAS:
+    // ---------------------------------------------------------------------------------------------------- //
+    // ПЕРВЫЙ ЭТАП ПАРСИНГА СТРОКИ В ВЫРАЖЕНИЕ (НА УРОВНЕ ОПЕРАЦИЙ В СООТВЕТСВИИ С PEMDAS)
+    // ---------------------------------------------------------------------------------------------------- //
 
-    std::unique_ptr<Node> parseExpression(const std::vector<std::string>& tokens, size_t& pos); // Парсинг выражения AS
-    std::unique_ptr<Node> parseTerm(const std::vector<std::string>& tokens, size_t& pos); // Парсинг терма MD
-    std::unique_ptr<Node> parseExponent(const std::vector<std::string>& tokens, size_t& pos); // Парсинг возведения в степень E
-    std::unique_ptr<Node> parseFactor(const std::vector<std::string>& tokens, size_t& pos); // Парсинг фактора P 
-                                                                                            // и атомарных элементов
-    // // ПАРСИНГ АТОМАРНЫХ ЭЛЕМЕНТОВ:     
+    /*
+    Сложение и вычитание.
+    */
+    std::unique_ptr<Node> parseExpression(const std::vector<std::string>&, size_t&);
 
-    std::unique_ptr<Node> parseNumber(const std::vector<std::string>& tokens, size_t& pos); // Парсинг числа
-    std::unique_ptr<Node> parseVariable(const std::vector<std::string>& tokens, size_t& pos); // Парсинг переменной
-    std::unique_ptr<Node> parseFunction(const std::vector<std::string>& tokens, size_t& pos); // Парсинг функции
+    /*
+    Умножение и деление.
+    */
+    std::unique_ptr<Node> parseTerm(const std::vector<std::string>&, size_t&);
 
-    // ДРУГИЕ ФУНКЦИИ
-    
-    T evaluateHelper(const Node* node) const;
-    std::unique_ptr<Node> copyNode(const Node* node) const;
-    std::unique_ptr<Node> subsVarHelper(std::unique_ptr<Node> node, 
-                                        const std::unordered_map<std::string, T>& varMap);
+    /*
+    Возведение в степень.
+    */
+    std::unique_ptr<Node> parseExponent(const std::vector<std::string>&, size_t&);
+
+    /*
+    Скобки.
+    */
+    std::unique_ptr<Node> parseFactor(const std::vector<std::string>&, size_t&);
+
+    // ---------------------------------------------------------------------------------------------------- //
+    // ВТОРОЙ ЭТАП ПАРСИНГА СТРОКИ В ВЫРАЖЕНИЕ (НА УРОВНЕ АТОМАРНЫХ ЭЛЕМЕНТОВ)
+    // ---------------------------------------------------------------------------------------------------- //
+
+    /*
+    Числа.
+    */
+    std::unique_ptr<Node> parseNumber(const std::vector<std::string>&, size_t&);
+
+    /*
+    Переменные.
+    */
+    std::unique_ptr<Node> parseVariable(const std::vector<std::string>&, size_t&);
+
+    /*
+    Функции.
+    */
+    std::unique_ptr<Node> parseFunction(const std::vector<std::string>&, size_t&);
+
+    // ---------------------------------------------------------------------------------------------------- //
+    // ВСПОМОГАТЕЛЬНЫЕ ФУНКЦИИ
+    // ---------------------------------------------------------------------------------------------------- //
+
+    /*
+    Вычисление значения выражения (основное тело).
+    */
+    T evaluateHelper(const Node*) const;
+
+    /*
+    Замена переменных в выражении (основное тело).
+    */
+    std::unique_ptr<Node> subsVarHelper(std::unique_ptr<Node>, const std::unordered_map<std::string, T>&);
+
+    /*
+    Дифференцирование выражения (основное тело).
+    */
+    std::unique_ptr<Node> differentiateHelper(const Node*, const std::string&) const;
+
+    /*
+    Копирование дерева.
+    */
+    std::unique_ptr<Node> copyTree(const Node*) const;
+
+    /*
+    Конвертация числа в строку.
+    */
+    static std::string numToString(const long double&);
+
+    /*
+    Конвертация одночлена в комплексное число.
+    */
+    static std::complex<long double> interpretComplex(const std::string& str);
 };
 
-#endif  
+std::ostream& operator<<(std::ostream&, const std::complex<long double>&);
+
+#endif
